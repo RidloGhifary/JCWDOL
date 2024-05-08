c1977bcae34c9156710578317f26169094778667
RidloGhifary <ridloghfry@gmail.com>
2024-05-08 21:43:10 +0700

    materi soal

--- fundamental/materi.js
+++ fundamental/materi.js
@@ -47,3 +47,58 @@
 // };
 
 // console.log(student?.fullName?.firstname); // Conditional Chaining
+
+// CLASS
+// Cetakan untuk Membuat Object dengan Property yg Sama
+
+// Case. Name, Price, Discount
+// const products = [
+//     { name: 'A', price: 5000, discount: 0 },
+//     { name: 'B', price: 10000, discount: 10 },
+//     { name: 'C', Price: 15000, discount: 15 }
+// ]
+
+// products.forEach(item => {
+//     console.log(`${item.name} = ${item.price}`)
+// })
+
+// class Products{
+//     name = ''
+//     price = 0
+//     discount = 0
+
+//     constructor(name, price, discount){
+//         this.name = name;
+//         this.price = price;
+//         this.discount = discount;
+//     }
+// }
+
+// const product1 = new Products('Apel', 10000, 10)
+// console.log(product1)
+
+// const arrProducts = [
+//     new Products('Apel', 10000, 10),
+//     new Products('Jeruk', 15000, 15),
+//     new Products('Anggur', 20000, 20)
+// ]
+
+// console.log(arrProducts)
+
+// Case. Sebuah Class untuk Data Student yang ada di Pwd
+class Student {
+  constructor(fullName, program, batch, gender) {
+    this.fullName = fullName;
+    this.program = program;
+    this.batch = batch;
+    this.gender = gender;
+  }
+
+  sayHello() {
+    return `Hallo saya ${this.fullName} dari ${this.program}-${this.batch}`;
+  }
+}
+
+const ridlo = new Student("ridlo achmad ghifary", "JWDOL", "14", "male");
+console.log(ridlo);
+console.log(ridlo.sayHello());
