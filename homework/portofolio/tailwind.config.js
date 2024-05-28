de02da5d5564e63587edaf657e246a2bd15fc006
RidloGhifary <ridloghfry@gmail.com>
2024-05-28 18:08:56 +0700

    done

--- homework/portofolio/tailwind.config.js
+++ homework/portofolio/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        AlmostBlack: "#111111",
+        AlmostWhite: "#cfd0d5",
+        SecondaryColor: "#e1a142",
+      },
+    },
   },
   plugins: [],
 };
