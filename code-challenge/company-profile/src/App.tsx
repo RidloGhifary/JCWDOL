af8b09e7069d8d2a63090e334cd22d9c5125524a
RidloGhifary <ridloghfry@gmail.com>
2024-06-01 15:01:56 +0700

    style: create header for home page

--- code-challenge/company-profile/src/App.tsx
+++ code-challenge/company-profile/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<p>Home page</p>} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<p>About page</p>} />
       <Route path="/cars" element={<p>Cars page</p>} />
       <Route path="/teams" element={<p>Teams page</p>} />
