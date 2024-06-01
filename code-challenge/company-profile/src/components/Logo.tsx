5ed28ee315eb42f42fa04e6184dac849094587cd
RidloGhifary <ridloghfry@gmail.com>
2024-06-01 14:29:42 +0700

    style: make navbar responsive on mobile

---
+++ code-challenge/company-profile/src/components/Logo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import LogoCompany from "../assets/logo.png";
+import { Link } from "react-router-dom";
+
+const Logo: React.FC = () => {
+  return (
+    <Link to="/" className="flex flex-1 items-center gap-1">
+      <img src={LogoCompany} alt="company-logo" width="56px" height="100%" />
+      <span className="hidden select-none text-xl font-semibold leading-tight text-mainColor md:block">
+        Car
+        <br />
+        Rental
+      </span>
+    </Link>
+  );
+};
+
+export default Logo;
