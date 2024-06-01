c5357229545007818b694f87595a594f712290c4
RidloGhifary <ridloghfry@gmail.com>
2024-06-01 18:53:34 +0700

    feat: add lazy load on every section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+
+const Home = React.lazy(() => import("./pages/Home"));
 
 const App: React.FC = () => {
   return (
