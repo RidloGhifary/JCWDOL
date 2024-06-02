8609c3dcc4482305e9773350da950b9cfeeaae33
RidloGhifary <ridloghfry@gmail.com>
2024-06-02 10:30:35 +0700

    chore: install lucide-react

--- code-challenge/company-profile/src/pages/About.tsx
+++ code-challenge/company-profile/src/pages/About.tsx
@@ -1,19 +1,24 @@
 import React from "react";
-import CompanyOverview from "../components/HomePage/CompanyOverview";
-import TimeLine from "../components/AboutPage/TimeLine";
-import Teams from "../components/Teams";
-import Values from "../components/AboutPage/Values";
+
+const CompanyOverview = React.lazy(
+  () => import("../components/HomePage/CompanyOverview"),
+);
+const TimeLine = React.lazy(() => import("../components/AboutPage/TimeLine"));
+const Teams = React.lazy(() => import("../components/Teams"));
+const Values = React.lazy(() => import("../components/AboutPage/Values"));
 
 const About: React.FC = () => {
   return (
     <React.Fragment>
-      <CompanyOverview
-        reverse={true}
-        additionalDesc="Car Rental was founded in 2015 by John Doe, who saw a gap in the market for a car rental company that truly prioritized customer convenience and satisfaction. Starting with a modest fleet of economy cars in a single location, the company quickly gained a reputation for reliability and excellent service."
-      />
-      <TimeLine />
-      <Teams />
-      <Values />
+      <React.Suspense fallback={<p className="text-center">Loading...</p>}>
+        <CompanyOverview
+          reverse={true}
+          additionalDesc="Car Rental was founded in 2015 by John Doe, who saw a gap in the market for a car rental company that truly prioritized customer convenience and satisfaction. Starting with a modest fleet of economy cars in a single location, the company quickly gained a reputation for reliability and excellent service."
+        />
+        <TimeLine />
+        <Teams />
+        <Values />
+      </React.Suspense>
     </React.Fragment>
   );
 };
